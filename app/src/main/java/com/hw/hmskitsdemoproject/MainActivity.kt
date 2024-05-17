246569bb39dc3441a250876e2a0797c54ae71c04
f00834653 <feyza.urkut1@huawei.com>
2024-05-17 11:27:36 +0300

    Request permissions and initialize MapsInitializer

--- app/src/main/java/com/hw/hmskitsdemoproject/MainActivity.kt
+++ app/src/main/java/com/hw/hmskitsdemoproject/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.hw.hmskitsdemoproject
 
+import android.content.pm.PackageManager
+import android.os.Build
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.huawei.hms.maps.MapsInitializer
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +23,52 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        MapsInitializer.initialize(this)
+        requestPermissions()
+
         // Go to Map Fragment
         supportFragmentManager.beginTransaction().replace(R.id.main, MapFragment()).commit()
     }
 
+    private fun requestPermissions() {
+        // Dynamically apply for required permissions if the API level is 28 or smaller.
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            //Log.i(TAG, "android sdk <= 28 Q")
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                val strings = arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+                ActivityCompat.requestPermissions(this, strings, 1)
+            }
+        } else {
+            // Dynamically apply for required permissions if the API level is greater than 28. The android.permission.ACCESS_BACKGROUND_LOCATION permission is required.
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                val strings = arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                )
+                ActivityCompat.requestPermissions(this, strings, 2)
+            }
+        }
+    }
 }
